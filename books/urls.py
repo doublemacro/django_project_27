5a6365e3d917904e0aca4846304e3f21266a98db
extaantochi <Ext.Aantochi@alzheimers.org.uk>
2026-07-16 20:53:03 +0300

    updated html structure, added base.html, added basic css to pages.

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.list_books, name='home'),
     path("create_book/", views.create_book, name='create_book'),
     path("delete_book/<int:pk>/", views.delete_book, name='delete_book'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
