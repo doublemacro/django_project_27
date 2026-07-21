940efa59b0d192d910430219aecc18cf3e5df5bd
extaantochi <Ext.Aantochi@alzheimers.org.uk>
2026-07-21 19:36:35 +0300

    added update to book CRUD

--- books/views.py
+++ books/views.py
@@ -36,6 +36,23 @@ def create_book(request: HttpRequest):
         list1 = [10, 20, 30, 40]
         return render(request, "books/book_form.html", context={"form": form, "list1": list1})
 
+def update_book(request: HttpRequest, pk: int):
+    book = get_object_or_404(Book, pk=pk)
+    # book = Book.objects.get(pk=pk)
+
+    if request.method == "POST":
+        # detaliile book-ului care au fost trimise de form folosind HTTP POST request, se afla in request.POST, ca un dictionar.
+        book_instance = BookForm(request.POST, instance=book)
+        if book_instance.is_valid():
+            # aici se updateaza un book in baza de date!
+            book_instance.save()
+            return redirect("home")
+    else:
+        # in cazul asta, request-ul poate fi GET, PUT, PATCH, DELETE, etc...
+        form = BookForm(instance=book)
+        return render(request, "books/update_book_form.html", context={"form": form})
+
+
 def delete_book(request: HttpRequest, pk: int):
     book = get_object_or_404(Book, pk=pk)
 
